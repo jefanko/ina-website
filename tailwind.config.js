3fe87e8b3fbf27c895800308f30b798b06648750
jefanko <61520081+jefanko@users.noreply.github.com>
2025-11-18 14:06:01 +0700

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'brand-primary': '#F77F00', // Warna utama Anda
+        'brand-dark': '#D97706', // Oranye yang lebih gelap & tenang untuk gradasi/hover
+      },
       backgroundImage: {
-        'transmission-lines': "url('/public/transmission-bg.png')",
+        'transmission-lines': "url('/transmission-bg.png')",
       },
     },
   },
