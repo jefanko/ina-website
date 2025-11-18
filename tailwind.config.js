340cc3d3c5e89d269c21b3b7304693a9e12e8c18
jefanko <61520081+jefanko@users.noreply.github.com>
2025-11-18 12:47:55 +0700

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'transmission-lines': "url('/transmission-bg.png')",
+        'transmission-lines': "url('/public/transmission-bg.png')",
       },
     },
   },
